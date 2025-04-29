25a40fb8bf1868a569461ef275e0224a4c80698a
Akkadate <89246175+Akkadate@users.noreply.github.com>
2025-04-29 23:50:44 +0700

    Update config.js

--- running-app-frontend/src/config.js
+++ running-app-frontend/src/config.js
@@ -1,1 +1,9 @@
-config.js
+// src/config.js
+const config = {
+  liffId: process.env.REACT_APP_LIFF_ID,
+  apiUrl: process.env.REACT_APP_API_URL,
+  supabaseUrl: process.env.REACT_APP_SUPABASE_URL,
+  supabaseAnonKey: process.env.REACT_APP_SUPABASE_ANON_KEY,
+};
+
+export default config;
