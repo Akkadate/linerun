e0603794c130967eb96a83e9d27d70f2ed6405a2
Akkadate <89246175+Akkadate@users.noreply.github.com>
2025-04-29 23:54:18 +0700

    Update Footer.js

--- running-app-frontend/src/components/common/Footer.js
+++ running-app-frontend/src/components/common/Footer.js
@@ -1,1 +1,23 @@
-Footer.js
+// src/components/common/Footer.js
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = styled.footer`
+  background-color: var(--text-color);
+  color: white;
+  padding: 1rem;
+  text-align: center;
+  font-size: 0.875rem;
+`;
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  
+  return (
+    <FooterContainer>
+      <p>&copy; {currentYear} LINE Running App. All rights reserved.</p>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
